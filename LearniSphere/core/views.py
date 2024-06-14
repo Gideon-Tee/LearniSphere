d6a301134eefea66fdca639cd25a4b385e25dd21
Gideon Tettehfio <gideonlambride@gmail.com>
2024-06-14 21:27:51 +0000

    search functionality implemented

--- LearniSphere/core/views.py
+++ LearniSphere/core/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from .models import Room, Topic
+from django.db.models import Q
 from .forms import RoomForm
 # Create your views here.
 
 
 def home(request):
-    rooms = Room.objects.all()
+    
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains = q) |
+        Q(name__icontains = q) |
+        Q(description__icontains = q)
+    )
+    topics = Topic.objects.all()
     context = {
-        'rooms': rooms
+        'rooms': rooms,
+        'topics': topics,
+        'rooms_count': rooms.count()
     }
     return render(request, 'core/home.html', context)
 
