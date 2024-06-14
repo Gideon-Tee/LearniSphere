2f3611b749c3ed4f58982998f5fb910b0ab6f8e2
Gideon Tettehfio <gideonlambride@gmail.com>
2024-06-14 23:06:37 +0000

    user login and logout functionality added

--- LearniSphere/core/urls.py
+++ LearniSphere/core/urls.py
@@ -6,6 +6,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('room/<int:pk>', views.room, name='room'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
     path('create-room', views.create_room, name='create-room'),
     path('update-room/<int:pk>', views.update_room, name='update-room'),
     path('delete-room/<int:pk>', views.delete_room, name='delete-room'),
--- LearniSphere/core/views.py
+++ LearniSphere/core/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import auth
+from django.contrib import messages
 from .models import Room, Topic
 from django.db.models import Q
 from .forms import RoomForm
 # Create your views here.
 
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'invalid credentials')
+    context = {}
+    return render(request, 'core/login_register.html', context)
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
 
 def home(request):
     
