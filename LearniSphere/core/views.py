38b31240d5c2fcbb691a2dc9b45f689e001bc87e
Gideon Tettehfio <gideonlambride@gmail.com>
2024-06-14 15:08:15 +0000

    CRUD operations on database

--- LearniSphere/core/views.py
+++ LearniSphere/core/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
+from .forms import RoomForm
 # Create your views here.
 
 
@@ -13,10 +14,44 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=int(pk))
-    # for room in rooms:
-    #     if room['id'] == int(pk):
-    #         Room = room
+
     context = {
         'room': room
     }
     return render(request, 'core/room.html', context)
+
+def create_room(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'core/room_form.html', context)
+
+
+def update_room(request, pk):
+    room = Room.objects.get(id=int(pk))
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {
+        'room': room,
+    }
+    return render(request, 'core/room_form.html', context)
+
+def delete_room(request, pk):
+    room = Room.objects.get(id=int(pk))
+
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+
+    return render(request, 'core/delete.html', {'obj': room.name})
